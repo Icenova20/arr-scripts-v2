a693d26ec2a330fd19fe9feaca466fab6a388e3f
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2025-12-09 15:14:14 -0500

    Add Radarr Unmapped Folder Cleaner script
    This script cleans up unmapped folders in Radarr by checking the API for unmapped folder paths and removing them if they exist. It also manages log files for the script's execution.

---
+++ Radarr-UnmappedFolderCleaner.bash
@@ -0,0 +1,94 @@
+#!/usr/bin/env bash
+scriptVersion="1.1"
+scriptName="Radarr-UnmappedFolderCleaner"
+dockerLogPath="/config/logs"
+
+settings () {
+  log "Import Script Settings..."
+  source /config/settings.conf
+  arrUrl="$radarrUrl"
+  arrApiKey="$radarrApiKey"
+}
+
+logfileSetup () {
+  logFileName="$scriptName-$(date +"%Y_%m_%d_%I_%M_%p").txt"
+
+  if [ ! -d "$dockerLogPath" ]; then
+    mkdir -p "$dockerLogPath"
+    chmod 777 "$dockerLogPath"
+  fi
+
+  if find "$dockerLogPath" -type f -iname "$scriptName-*.txt" | read; then
+    # Keep only the last 5 log files for 6 active log files at any given time...
+    rm -f $(ls -1t $dockerLogPath/$scriptName-* | tail -n +5)
+    # delete log files older than 5 days
+    find "$dockerLogPath" -type f -iname "$scriptName-*.txt" -mtime +5 -delete
+  fi
+  
+  if [ ! -f "$dockerLogPath/$logFileName" ]; then
+    echo "" > "$dockerLogPath/$logFileName"
+    chmod 666 "$dockerLogPath/$logFileName"
+  fi
+}
+
+log () {
+  m_time=`date "+%F %T"`
+  echo $m_time" :: $scriptName (v$scriptVersion) :: "$1
+  echo $m_time" :: $scriptName (v$scriptVersion) :: "$1 >> "$dockerLogPath/$logFileName"
+}
+
+verifyConfig () {
+
+  if [ "$enableUnmappedFolderCleaner" != "true" ]; then
+    log "Script is not enabled, enable by setting enableUnmappedFolderCleaner to \"true\" by modifying the \"/config/extended.conf\" config file..."
+    log "Sleeping (infinity)"
+    sleep infinity
+  fi
+
+}
+
+UnmappedFolderCleanerProcess () {
+	log "Finding UnmappedFolders to purge..."
+	OLDIFS="$IFS"
+	IFS=$'\n'
+    unmappedFoldersCount=$(curl -s "$arrUrl/api/v3/rootFolder" -H "X-Api-Key: $arrApiKey" | jq -r ".[].unmappedFolders[].path" | wc -l)
+	log "$unmappedFoldersCount Folders Found!"
+	if [ $unmappedFoldersCount = 0 ]; then 
+	    log "No cleanup required, exiting..."
+	    return
+	fi
+    unmappedFolders=$(curl -s "$arrUrl/api/v3/rootFolder" -H "X-Api-Key: $arrApiKey" | jq -r ".[].unmappedFolders[].path")
+	for folder in $(echo "$unmappedFolders"); do
+	    log "Removing $folder"
+		if [ -d "$folder" ]; then
+	    	rm -rf "$folder"
+		else
+			log "ERROR :: Cannot Delete \"$foler\", directory not found, skipping..."
+      log "ERROR :: Check to make sure Radarr root folder is mapped properly to this container..."
+		fi
+	done
+	IFS="$OLDIFS"
+ }
+
+
+# Loop Script
+for (( ; ; )); do
+	let i++
+	logfileSetup
+	log "Starting..."
+	settings
+	verifyConfig
+  if [ ! -z "$radarrUrl" ]; then
+    if [ ! -z "$radarrApiKey" ]; then
+	    UnmappedFolderCleanerProcess
+    else
+      log "ERROR :: Skipping, missing API Key..."
+    fi
+  else
+    log "ERROR :: Skipping, missing URL..."
+  fi
+	log "Script sleeping for $unmappedFolderCleanerScriptInterval..."
+	sleep $unmappedFolderCleanerScriptInterval
+done
+
+exit
