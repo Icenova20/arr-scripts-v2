65f06dbde90baaaf09074fcace69a72e5854b4f8
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2025-12-11 15:12:02 -0500

    Update script version and logging for daily series

--- Sonarr-DailySeriesEpisodeTrimmer.bash
+++ Sonarr-DailySeriesEpisodeTrimmer.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-scriptVersion="1.0"
+scriptVersion="1.1"
 scriptName="Sonarr-DailySeriesEpisodeTrimmer"
 dockerLogPath="/config/logs"
 
@@ -49,7 +49,7 @@ verifyConfig () {
 DailySeriesTrimmerProcess () {
     log "Get Sonarr Series List"
     sonarrSeriesList=$(curl -s --header "X-Api-Key:"${arrApiKey} --request GET  "$arrUrl/api/v3/series")
-    sonarrSeriesIds=$(echo "${sonarrSeriesList}" | jq -r '.[] | select(.seriesType=="daily") |.id')
+    sonarrSeriesIds=$(echo "${sonarrSeriesList}" | jq -r '.[] |.id')
     sonarrSeriesTotal=$(echo "${sonarrSeriesIds}" | wc -l)
     loopCount=0
     for id in $(echo $sonarrSeriesIds); do
@@ -59,12 +59,6 @@ DailySeriesTrimmerProcess () {
         seriesTitle=$(echo $seriesData | jq -r ".title")
         seriesType=$(echo $seriesData | jq -r ".seriesType")
         seriesTags=$(echo $seriesData | jq -r ".tags[]")
-        seriesEpisodeData=$(curl -s "$arrUrl/api/v3/episode?seriesId=$seriesId&apikey=$arrApiKey")
-        seriesEpisodeIds=$(echo "$seriesEpisodeData" | jq -r " . | sort_by(.airDate) | reverse | .[] | select(.hasFile==true) | .id")
-        seriesEpisodeIdsCount=$(echo "$seriesEpisodeIds" | wc -l)
-
-        log "$seriesId"
-
         # If sonarr series is tagged, match via tag to support series that are not considered daily
         if [ -z "$sonarrSeriesEpisodeTrimmerTag" ]; then
             tagMatch="false"
@@ -81,19 +75,23 @@ DailySeriesTrimmerProcess () {
 
         # Verify series is marked as "daily" type by sonarr, skip if not...
         if [ $seriesType != "daily" ] && [ "$tagMatch" == "false" ]; then
-            log "$seriesTitle (ID:$seriesId) :: ERROR :: Series does not match TYPE: Daily or TAG: $sonarrSeriesEpisodeTrimmerTag, skipping..."
-            exit
+            log "$loopCount/$sonarrSeriesTotal :: $seriesTitle (ID:$seriesId) :: ERROR :: Series does not match TYPE: Daily or TAG: $sonarrSeriesEpisodeTrimmerTag, skipping..."
+            continue
         fi
+
+        seriesEpisodeData=$(curl -s "$arrUrl/api/v3/episode?seriesId=$seriesId&apikey=$arrApiKey")
+        seriesEpisodeIds=$(echo "$seriesEpisodeData" | jq -r " . | sort_by(.airDate) | reverse | .[] | select(.hasFile==true) | .id")
+        seriesEpisodeIdsCount=$(echo "$seriesEpisodeIds" | wc -l)
 
         # If non-daily series, set maximum episode count to match latest season total episode count
         if [ $seriesType != "daily" ]; then
-        maximumDailyEpisodes=$(echo "$seriesData" | jq -r ".seasons | sort_by(.seasonNumber) | reverse | .[].statistics.totalEpisodeCount" | head -n1)
+            maximumDailyEpisodes=$(echo "$seriesData" | jq -r ".seasons | sort_by(.seasonNumber) | reverse | .[].statistics.totalEpisodeCount" | head -n1)
         fi
 
         # Skip processing if less than the maximumDailyEpisodes setting were found to be downloaded
         if [ $seriesEpisodeIdsCount -lt $maximumDailyEpisodes ]; then
-            log "$seriesTitle (ID:$seriesId) :: ERROR :: Series has not exceeded $maximumDailyEpisodes downloaded episodes ($seriesEpisodeIdsCount files found), skipping..."
-            exit
+            log "$loopCount/$sonarrSeriesTotal :: $seriesTitle (ID:$seriesId) :: ERROR :: Series has not exceeded $maximumDailyEpisodes downloaded episodes ($seriesEpisodeIdsCount files found), skipping..."
+            continue
         fi
 
         # Begin processing "daily" series type
@@ -113,21 +111,21 @@ DailySeriesTrimmerProcess () {
                 episodeFileId=$(echo "$episodeData" | jq -r ".episodeFileId")
                 
                 # Unmonitor downloaded episode if greater than 14 downloaded episodes
-                log "$seriesTitle (ID:$episodeSeriesId) :: S${episodeSeasonNumber}E${episodeNumber} :: $episodeTitle :: Unmonitored Episode ID :: $id"
+                log "$loopCount/$sonarrSeriesTotal :: $seriesTitle (ID:$episodeSeriesId) :: S${episodeSeasonNumber}E${episodeNumber} :: $episodeTitle :: Unmonitored Episode ID :: $id"
                 umonitorEpisode=$(curl -s "$arrUrl/api/v3/episode/monitor?apikey=$arrApiKey" -X PUT -H 'Content-Type: application/json'  --data-raw "{\"episodeIds\":[$id],\"monitored\":false}")
 
                 # Delete downloaded episode if greater than 14 downloaded episodes
-                log "$seriesTitle (ID:$episodeSeriesId) :: S${episodeSeasonNumber}E${episodeNumber} :: $episodeTitle :: Deleted File ID :: $episodeFileId"
+                log "$loopCount/$sonarrSeriesTotal :: $seriesTitle (ID:$episodeSeriesId) :: S${episodeSeasonNumber}E${episodeNumber} :: $episodeTitle :: Deleted File ID :: $episodeFileId"
                 deleteFile=$(curl -s "$arrUrl/api/v3/episodefile/$episodeFileId?apikey=$arrApiKey" -X DELETE)
                 seriesRefreshRequired=true
             else
                 # Skip if less than required 14 downloaded episodes exist
-                log "$seriesTitle (ID:$episodeSeriesId) :: Skipping Episode ID :: $id"
+                log "$loopCount/$sonarrSeriesTotal :: $seriesTitle (ID:$episodeSeriesId) :: Skipping Episode ID :: $id"
             fi
         done
         if [ "$seriesRefreshRequired" = "true" ]; then
             # Refresh Series after changes
-            log "$seriesTitle (ID:$episodeSeriesId) :: Refresh Series"
+            log "$loopCount/$sonarrSeriesTotal :: $seriesTitle (ID:$episodeSeriesId) :: Refresh Series"
             refreshSeries=$(curl -s "$arrUrl/api/v3/command?apikey=$arrApiKey" -X POST --data-raw "{\"name\":\"RefreshSeries\",\"seriesId\":$episodeSeriesId}")
         fi
     done
